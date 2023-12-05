f114ec2317b05118e949fb1afde58e8a46025ff1
jtrichWM <144946932+jtrichWM@users.noreply.github.com>
2023-12-05 11:32:27 -0500

    Update Databased_DDL.sql

--- Databased_DDL.sql
+++ Databased_DDL.sql
@@ -11,3 +11,19 @@ CREATE TABLE employees (
     department VARCHAR(50),
     salary DECIMAL(10, 2)
 );
+
+CREATE TABLE agency (
+    name VARCHAR(50) PRIMARY KEY,
+    planet VARCHAR(50)
+);
+
+CREATE TABLE planet (
+    name VARCHAR(50) PRIMARY KEY,
+    galaxy VARCHAR(50),
+    climate VARCHAR(50)
+);
+
+CREATE TABLE orphanages (
+    name VARCHAR(50) PRIMARY KEY,
+    agency VARCHAR(50) PRIMARY KEY
+);

cca15a8a7cd2cad03fc60650b47346edca9d9cb0
Connor MacKinnon <89928233+cdmackinnon@users.noreply.github.com>
2023-12-05 11:38:42 -0500

    Create Databased_small.sql

---
+++ Databased_small.sql
@@ -0,0 +1,5 @@
+-- Project 2 Alien Adoption Table Population
+-- By Connor MacKinnon, Jake Richardson, Hannah Zimmerman
+-- SQL statements to populate a small example of your database
+-- The inserted tuples must exercise all integrity constraints of your design
+

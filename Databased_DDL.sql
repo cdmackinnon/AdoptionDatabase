92b481dcfe5bc0653de9667f390c2017654639a7
jtrichWM <144946932+jtrichWM@users.noreply.github.com>
2023-12-05 11:33:59 -0500

    Comment Sample Table

--- Databased_DDL.sql
+++ Databased_DDL.sql
@@ -2,15 +2,15 @@
 -- By Connor MacKinnon, Jake Richardson, Hannah Zimmerman
 
 --Sample Table Generation ;)
-CREATE TABLE employees (
-    employee_id INT PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    birth_date DATE,
-    hire_date DATE,
-    department VARCHAR(50),
-    salary DECIMAL(10, 2)
-);
+--CREATE TABLE employees (
+--    employee_id INT PRIMARY KEY,
+--    first_name VARCHAR(50),
+--    last_name VARCHAR(50),
+--    birth_date DATE,
+--    hire_date DATE,
+--    department VARCHAR(50),
+--    salary DECIMAL(10, 2)
+--);
 
 CREATE TABLE agency (
     name VARCHAR(50) PRIMARY KEY,

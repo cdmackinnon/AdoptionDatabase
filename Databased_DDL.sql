7f7f0db96aacb4959e4583f420f0c239404323ac
Connor MacKinnon <89928233+cdmackinnon@users.noreply.github.com>
2023-12-07 12:01:21 -0500

    Adjust Order

--- Databased_DDL.sql
+++ Databased_DDL.sql
@@ -1,26 +1,18 @@
 -- Project 2 Alien Adoption Table Instantiation
 -- By Connor MacKinnon, Jake Richardson, Hannah Zimmerman
 
+CREATE TABLE Planet (
+    name VARCHAR(50) PRIMARY KEY,
+    galaxy VARCHAR(50) NOT NULL,
+    climate VARCHAR(50) NOT NULL
+);
+
+
 CREATE TABLE Family(
     ID SERIAL PRIMARY KEY,
     last_name VARCHAR(50) NOT NULL,
     income DECIMAL(10, 2)
 );
-
-CREATE TABLE Inhabits (
-    alien_id  PRIMARY KEY REFERENCES Alien(ID),
-    planet REFERENCES Planet(name)  --a families planet may be null if they are on a spacecraft
-        ON DELETE SET NULL
-);
-
-CREATE TABLE Adoption_request (
-     request_id SERIAL PRIMARY KEY,
-     alien_id INT REFERENCES alien(alien_id)
-        ON DELETE CASCADE,
-     family_id INT REFERENCES family(family_id)
-        ON DELETE CASCADE,
-     UNIQUE (alien_id, family_id) -- Ensures a family cannot request the same alien twice
- );
 
 CREATE TABLE Agency (
     name VARCHAR(50) PRIMARY KEY,
@@ -28,21 +20,11 @@ CREATE TABLE Agency (
         ON DELETE SET NULL
 );
 
-CREATE TABLE Planet (
-    name VARCHAR(50) PRIMARY KEY,
-    galaxy VARCHAR(50) NOT NULL,
-    climate VARCHAR(50) NOT NULL
-);
 
 CREATE TABLE Orphanages (
-    name VARCHAR(50) PRIMARY KEY,
-    agency VARCHAR(50) PRIMARY KEY --different agencies may have the same named orphanages
-);
-
-CREATE TABLE Medical (
-    alien_id INT PRIMARY KEY REFERENCES Alien (ID),
-    age INT,
-    vaccinated BOOLEAN
+    name VARCHAR(50),
+    agency VARCHAR(50) REFERENCES Agency(name), --different agencies may have the same named orphanages
+    PRIMARY KEY (name, agency)
 );
 
 CREATE TABLE Alien (
@@ -55,11 +37,36 @@ CREATE TABLE Alien (
 );
 
 
+CREATE TABLE Medical (
+    alien_id INT PRIMARY KEY REFERENCES Alien (ID),
+    age INT,
+    vaccinated BOOLEAN
+);
+
+
 CREATE TABLE Home_planet (
     alien_id INT PRIMARY KEY REFERENCES Alien(ID),
     planet REFERENCES Planet(name)
         ON DELETE SET NULL
 );
+
+
+CREATE TABLE Inhabits (
+    alien_id INT PRIMARY KEY REFERENCES Alien(ID),
+    planet REFERENCES Planet(name)  --a families planet may be null if they are on a spacecraft
+        ON DELETE SET NULL
+);
+
+
+CREATE TABLE Adoption_request (
+     request_id SERIAL PRIMARY KEY,
+     alien_id INT REFERENCES alien(alien_id)
+        ON DELETE CASCADE,
+     family_id INT REFERENCES family(family_id)
+        ON DELETE CASCADE,
+     UNIQUE (alien_id, family_id) -- Ensures a family cannot request the same alien twice
+ );
+
 
 CREATE TABLE Adopted (
     alien_id INT PRIMARY KEY REFERENCES Alien(ID) 
